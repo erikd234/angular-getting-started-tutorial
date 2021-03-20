d7f8afa42a210fd2c3500f024f8856dcc064accb
erikd234 <erikd234@gmail.com>
2021-03-20 14:00:11 -0700

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent {
   products = products;
-
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale.");
+  }
   share() {
-    window.alert('The product has been shared!');
+    window.alert("The product has been shared!");
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
